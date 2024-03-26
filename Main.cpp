97a1e467cb92fcbd9904f91e3c222d8448e9adfe
mbaecker2003 <148389448+mbaecker2003@users.noreply.github.com>
2024-03-26 13:16:04 +0100

    Update wrong paths and add comments

--- Main.cpp
+++ Main.cpp
@@ -1,18 +1,44 @@
-#include <stdio.h>
+#include <iostream>
 #include <opencv2/opencv.hpp>
+#include<vector>
+#include <array>
+
 
 using namespace cv;
+using namespace std;
+
 int main(int argc, char** argv )
 {
-
-    Mat image;
-    image = imread("C:/Users/marvi/Desktop/Projekt/b/lenna.png");
-    if ( !image.data )
+    // Load Image
+    Mat image = imread("C:/Users/marvi/Desktop/Projekt/Computer_Vision/lenna.png");
+    // Check if the image was loaded successfully
+    if (!image.data)
     {
-        printf("No image data \n");
+        cout<<("No image is found")<<endl;
         return -1;
     }
-    namedWindow("Display Image", WINDOW_AUTOSIZE );
+
+    // Image attributes
+    int imageRows = image.rows;
+    int imageColumns = image.cols;
+
+    // Vector to store pixle information
+    vector<vector<array<int, 3>>> pixles(imageRows, vector<array<int, 3>>(imageColumns));
+
+    // Get RGB-values of the image
+    for(int x=0; x<imageColumns; x++){
+        for(int y=0; y<imageRows; y++){
+            Vec3d pixleVector = image.at<Vec3d>(y,x);
+
+            // Save {0:Red, 1:Green, 2:Blue} Color value as an int-Array
+            pixles[x][y] = {(int) pixleVector[2], (int) pixleVector[1], (int) pixleVector[0]};
+        }
+    }
+
+
+
+
+    namedWindow("Display Image", WINDOW_AUTOSIZE);
     imshow("Display Image", image);
     waitKey(0);
     return 0;
